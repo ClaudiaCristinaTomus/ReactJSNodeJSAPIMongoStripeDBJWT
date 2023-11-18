ba48ec931173c8038ddb72032483d1955e05085f
ClaudiaCristinaTomus <tomus_clau@yahoo.com>
2023-11-18 17:08:24 +0200

    second commit

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -34,7 +34,8 @@ const Arrow = styled.div`
 const Wrapper = styled.div`
   height: 100%;
   display: flex;
-  transform: translateX(0vw);
+  transition: all 1.5s ease;
+  transform: translateX(${props=>props.slideIndex * -100}vw);
 `;
 
 const Slide = styled.div`
@@ -42,7 +43,7 @@ const Slide = styled.div`
   height: 100vh;
   display: flex;
   align-items: center;
-  background-color: #${props=>props.bg};
+  background-color: #${(props) => props.bg};
 `;
 
 const ImgContainer = styled.div`
@@ -63,53 +64,49 @@ const Title = styled.h1`
   font-size: 70px;
 `;
 const Desc = styled.p`
-  margin:50px 0px;
+  margin: 50px 0px;
   font-size: 20px;
   font-weight: 500;
   letter-spacing: 3px;
 `;
 const Button = styled.button`
-padding:10px;
-font-size: 20px;
-background-color: transparent;
-cursor: pointer;
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
 `;
 
 const Slider = () => {
-    const[slideIndex, setSlideIndex] = useState(0);
-    const handleClick = (direction) => {
-        if(direction === "left"){
-            setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
-        }
-        else{
-            setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
-        }
-    };
+  const [slideIndex, setSlideIndex] = useState(0);
+  const handleClick = (direction) => {
+    if (direction === "left") {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+    } else {
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+    }
+  };
   return (
     <Container>
-      <Arrow direction="left" onClick={()=>handleClick("left")}>
+      <Arrow direction="left" onClick={() => handleClick("left")}>
         <ArrowLeftOutlined />
       </Arrow>
-      <Wrapper>
-        {sliderItems.map(item=>(
-             <Slide bg={item.bg}>
-             <ImgContainer>
-               <Image src={item.img} />
-             </ImgContainer>
-             <InfoContainer>
-               <Title>{item.title}</Title>
-               <Desc>
-                {item.desc}
-               </Desc>
-               <Button>SHOP NOW</Button>
-             </InfoContainer>
-           </Slide>
+      <Wrapper slideIndex={slideIndex}>
+        {sliderItems.map((item) => (
+          <Slide bg={item.bg} key={item.id}>
+            <ImgContainer>
+              <Image src={item.img} />
+            </ImgContainer>
+            <InfoContainer>
+              <Title>{item.title}</Title>
+              <Desc>{item.desc}</Desc>
+              <Button>SHOP NOW</Button>
+            </InfoContainer>
+          </Slide>
         ))}
-       
 
         <Slide bg="fbf9f4">
           <ImgContainer>
-            <Image src="https://time.com/shopping/static/fbe92fc9c4467fd478f4786e9d12cd51/4cda9/lulus-wedding-guest-dresses1.jpg"/>
+            <Image src="https://time.com/shopping/static/fbe92fc9c4467fd478f4786e9d12cd51/4cda9/lulus-wedding-guest-dresses1.jpg" />
           </ImgContainer>
           <InfoContainer>
             <Title>Summer SALE</Title>
@@ -122,7 +119,7 @@ const Slider = () => {
 
         <Slide bg="fbf0f8">
           <ImgContainer>
-            <Image src="https://assets.vogue.com/photos/64b98da1c149451f30a3e87e/master/w_1600%2Cc_limit/22SS5454_IVRY_1.jpg"/>
+            <Image src="https://assets.vogue.com/photos/64b98da1c149451f30a3e87e/master/w_1600%2Cc_limit/22SS5454_IVRY_1.jpg" />
           </ImgContainer>
           <InfoContainer>
             <Title>Summer SALE</Title>
@@ -133,7 +130,7 @@ const Slider = () => {
           </InfoContainer>
         </Slide>
       </Wrapper>
-      <Arrow direction="right" onClick={()=>handleClick("right")}>
+      <Arrow direction="right" onClick={() => handleClick("right")}>
         <ArrowRightOutlined />
       </Arrow>
     </Container>
--- src/data.js
+++ src/data.js
@@ -21,4 +21,48 @@ export const sliderItems = [
         bg:"f5fafd"
     },
 
-]
+];
+
+export const categories = [
+    {
+        id:1,
+        img:"https://assets.vogue.com/photos/648a1fc627067feaf30025ab/master/w_1600%2Cc_limit/CLARIBELLE%2520DRESS_BLANC%2520BLUEBELL%2520BALLAD_01.jpg",
+        title: "SHIRT STYLE",
+       
+    },
+    {
+        id:2,
+        img:"https://time.com/shopping/static/fbe92fc9c4467fd478f4786e9d12cd51/4cda9/lulus-wedding-guest-dresses1.jpg",
+        title: "LOUNGEWEAR LOVE",
+        
+    },
+    {
+        id:3,
+        img:"https://assets.vogue.com/photos/64b98da1c149451f30a3e87e/master/w_1600%2Cc_limit/22SS5454_IVRY_1.jpg",
+        title: "LIGHT JACKETS",
+        
+    },
+
+];
+
+export const popularProducts = [
+    {
+        id:1,
+        img:"https://assets.vogue.com/photos/648a1fc627067feaf30025ab/master/w_1600%2Cc_limit/CLARIBELLE%2520DRESS_BLANC%2520BLUEBELL%2520BALLAD_01.jpg",
+        
+       
+    },
+    {
+        id:2,
+        img:"https://time.com/shopping/static/fbe92fc9c4467fd478f4786e9d12cd51/4cda9/lulus-wedding-guest-dresses1.jpg",
+        
+        
+    },
+    {
+        id:3,
+        img:"https://assets.vogue.com/photos/64b98da1c149451f30a3e87e/master/w_1600%2Cc_limit/22SS5454_IVRY_1.jpg",
+        
+        
+    },
+
+];
